7a0f6afadba18864755c518125deb050fba28b74
Mursaleen00 <playessport@gmail.com>
2023-09-06 20:32:51 +0500

    Check

---
+++ 9-6-23/1.js
@@ -0,0 +1,18 @@
+//  question url
+// https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+
+
+// Sample Tast
+// const Test = require('@codewars/test-compat');
+// describe("Tests", () => {
+//     it("test", () => {
+//         Test.assertDeepEquals(mystery(), { sanity: 'Hello' }, 'Mystery has not returned to sanity');
+//     });
+// });
+
+// My Answer
+function mystery() {
+    var results =
+        { sanity: 'Hello' };
+    return results;
+}
